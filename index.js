6505f4dfb34bf6a712e58739f760b6cd344a2187
girija <ayushmatichakara@gmail.com>
2022-07-02 03:17:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.mytext' , {
+    strings : ["JavaScript Animations"],
+    typeSpeed : 50,
+    loop : false,
+    backDelay : 900,
+    backSpeed : 100
+})
+
+AOS.init({
+    duration : 2000
+})
